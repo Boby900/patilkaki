c69bd753e9f97710082b7bc43fafb4c6b5499855
Boby900 <bobytiwari310@gmail.com>
2024-02-13 09:24:07 +0530

    added license

---
+++ src/app/founder/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Founder = () => {
+  return (
+    <div>
+        <h1>About Founder X</h1>
+        <p>The world needs your creativity and igenuity. Don&#39;t put your dreams on hold. <br /> Start your founder&#39;s journey today!</p>
+        <div>
+            <div>a</div>
+            <div>b</div>
+        </div>
+    </div>
+  )
+}
+
+export default Founder
